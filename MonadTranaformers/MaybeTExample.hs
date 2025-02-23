4eb85a4d73e42608eb8b39bcfc001d9b2f45e020
gmlion <gmlion@gmail.com>
2025-02-23 21:59:07 +0100

    lift example

--- MonadTranaformers/MaybeTExample.hs
+++ MonadTranaformers/MaybeTExample.hs
@@ -1,8 +1,9 @@
 -- Examples adapted from "Monad Transformers are good, actually" by Gabriella Gonzalez
 -- https://youtu.be/w9ExsWcoXPs?si=FGf1t7lH5oHWkdXf
 
-import Text.Read
+import qualified Text.Read
 import Control.Applicative ( Alternative((<|>)) )
+import Control.Monad.Trans
 import Control.Monad.Trans.Maybe 
 -- Usually MTL (Monad Transformers Library) is imported. It aggregates and exposes several functionalities
 -- using the transformers library behind the scenes.
@@ -12,7 +13,7 @@ import Control.Monad.Trans.Maybe
 readInt :: IO (Maybe Int)
 readInt = do
     input <- getLine
-    return (readMaybe input)
+    return (Text.Read.readMaybe input)
 
 -- Imagine we want to parse three ints consecutively, continuing only in the Just case
 -- We should pattern match inside the IO do notation, but we can "merge" the two monads and
@@ -40,4 +41,17 @@ ensureInt' = runMaybeT loop
         loop = MaybeT readInt <|> loop
                         -- Our top level monad is IO, but the Alternative instance used here
                         -- is the one of Maybe
+
+-- We may want to "interact" with the original monad (IO in this case)
+readAndPrintTwoInts = runMaybeT maybeIO
+    where
+        maybeIO = do
+            int0 <- MaybeT readInt
+            MaybeT (fmap Just (print int0))     -- print int0 :: IO ()
+                                                -- Just (print int0) :: Maybe IO ()
+                                                -- fmap Just (print int0) :: IO Maybe ()
+                                                -- MaybeT (fmap Just (print int0)) :: MaybeT IO ()
+            int1 <- MaybeT readInt
+            lift (print int1)                   -- Same as above. We "lift" the IO to MaybeT with no intermediate Maybe to manage
+
     
